1967f11211dac232b74b4822f9cff2d97c5d0c8e
AntiPlexz <bmxredeath@gmail.com>
2025-08-17 00:13:11 -0500

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Update package lists and install PostgreSQL headers for psycopg2 compilation
-apt-get update
-apt-get install -y libpq-dev
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
